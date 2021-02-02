6179f132a4d2ca2aa35c1726c23edaa9900e75ee
soootaleb <talebs@icloud.com>
2021-02-02 23:51:15 +0100

    cleans & demonstrates messages onboarding

--- src/messages.ts
+++ src/messages.ts
@@ -2,6 +2,7 @@ import Observe from "https://deno.land/x/Observe/Observe.ts";
 
 enum MT {
     Ping = "Ping",
+    Pong = "Pong",
     LogMessage = "LogMessage",
     InitialMessage = "InitialMessage",
     MessageRequest = "MessageRequest",
@@ -12,9 +13,10 @@ enum MT {
  * List MessageTypes along with their Payloads
  */
 type Mapping = {
-    [MT.Ping]: {},
-    [MT.InitialMessage]: {},
-    [MT.LogMessage]: { message: string }
+    [MT.Pong]: null, // Empty payloads...
+    [MT.Ping]: null, // Empty payloads...
+    [MT.InitialMessage]: null,
+    [MT.LogMessage]: { message: string } // Simple payloads
     [MT.MessageRequest]: {
         key: string,
         value: string
@@ -37,31 +39,19 @@ type Message<T extends keyof Mapping> = {
     payload: Mapping[T]
 }
 
+/**
+ * Verify extends vs in keyof, ...
+ */
 type H<T extends MT> = (message: Message<T>) => void
 
-/**
- * Generic send()
- * - Ensures the type is from Mapping (autocomplete)
- * - Ensures the payload is the correct one
- * - Automates the definition or source
- */
 class IO extends Object {
 
     /**
      * Doesn't seem to work as intended (check the constructor, payload doesn't match MType)
+     * !!! Take a time to console.log(messages), very interesting props
      */
     private messages: Observe<Message<keyof Mapping>>;
 
-    // protected handlers: {
-    //     [key in MT]?: H<key>
-    // } = {}
-
-    /**
-     * Trying very hard to declare keys of type MType associated with Handlers (like in a type)
-     * This to avoid duplication of MType in property name along with typing the property
-     * For now duplication is mandatory to get typing of message in callback
-     */
-    // [Symbol.for(key in MT)]: {};
 
     constructor(messages: Observe<Message<keyof Mapping>>) {
         super();
@@ -81,130 +71,65 @@ class IO extends Object {
         })
     }
 
-    protected send<T extends keyof Mapping>(type: T, payload: Mapping[T], destination: string) {
-        const message = {
+    protected send<T extends MT>(type: T, payload: Mapping[T], destination: string) {
+        this.messages.setValue({
             type: type,
             source: this.constructor.name,
             destination: destination.toUpperCase().substring(0, 1) + destination.substring(1),
             payload: payload
-        };
-        console.log("[SENDING]", message)
-        this.messages.setValue(message)
+        })
     }
-
-    /**
-     * Each .receive() must be called during object initialization
-     * Hence, .receive() would be used in methods & those methods would stack in the constructor
-     * @param type 
-     * @param callback 
-     */
-    // protected receive<T extends keyof Mapping>(type: T, callback: (message: {
-    //     type: T,
-    //     source: string,
-    //     destination: string,
-    //     payload: Mapping[T]
-    // }) => void) {
-    //     // What ?
-    //     // Registering the callback OK
-    //     // But most importantly, .receive() must be called (registered)
-    // }
 }
 
 /**
- * Extend IO & call this.send(MType, Payload, Destination)
- * - MessageType is forced (+ autocomplete)
- * - MessagePayload is aligned with message type
- * - Destination is just after, non-intrusive
+ * A components extends IO
+ * - uses this.send() to send typed messages
+ * - declares [MT.MType] to handle typed messages
  */
 class Node extends IO {
 
     constructor(messages: Observe<Message<keyof Mapping>>) {
         super(messages);
-
-        /**
-         * With this approach I have to call this.receive() for each MessageType
-         */
-        // this.receive(MT.MessageRequest, (message) => {
-        //     console.log("Received typed message", message);
-        //     console.log("Message from ", message.source);
-        //     console.log("Payload contains ", message.payload.complex)
-        // })
-    }
-
-    /**
-     * Ideally, no call is needed;
-     * However, MessageType is duplicated & must be SYNC
-     */
-    // protected handlers: { [key in MT]?: H<key> } = {
-
-    //     // This is automatically called by parent IO in case of MessageType & Destintation match
-    //     [MT.MessageRequest]: (message) => {
-
-    //         console.log(this.isPrototypeOf({})) // Just to check this access
-    //         console.log("Message from ", message.source);
-    //         console.log("Payload contains ", message.payload.complex)
-
-    //     }
-
-    // }
-
-    /**
-     * This is a better way
-     * still duplication
-     * but not in a "handler" object
-     * 
-     * /!!!\ this.messages has VERY INTERESTING PROPS (history, eventID, boundCallbacks, current, ...)
-     */
-    protected [MT.LogMessage]: H<MT.LogMessage> = (message) => {
-        console.log("[RECEIVED]", {
-            self: this,
-            message: message.payload.message,
-            complete: message
-        })
-    }
-
-    public ping() {
-        this.send(MT.Ping, {}, "Store")
-    }
-
-}
-
-/**
- * VERY GOOD WAY TO TYPE THE SENDING OF A MESSAGE
- * - WHAT'S THE EQUIVALENT FOR RECEIVING MESSAGE?
- * - HOW TO MAKE SURE SENDING & RECEIVING ARE ALIGNED?
- */
-class Store extends IO {
-    constructor(messages: Observe<Message<keyof Mapping>>) {
-        super(messages);
-        this.send(MT.LogMessage, { message: "Howdie !" }, "node")
     }
 
     protected [MT.Ping]: H<MT.Ping> = (message) => {
-        this.send(MT.Ping, {}, "Node")
+        this.send(MT.Pong, null, message.source);
     }
 }
 
-// function receive(type: keyof Mapping) {
-//     return (proto: Object, method: string, descriptor: PropertyDescriptor) => {
-//         descriptor.value = function(...args: any[]) {
-//             console.log(proto, method, descriptor, args, type)
-//             return descriptor.value.apply(this, args);
-//         }
-//     }
-// }
+class Net extends IO {
+    constructor(messages: Observe<Message<keyof Mapping>>) {
+        super(messages);
+    }
 
+    [MT.Pong]: H<MT.Pong> = (message) => {
+        this.send(MT.LogMessage, {
+            message: "Received pong from " + message.source
+        }, "Log")
+    }
+
+    public ping() {
+        this.send(MT.Ping, null, "Node")
+    }
+}
+
+// Init messages
 const messages = new Observe({
     type: MT.LogMessage,
     source: "Root",
-    destination: "Store",
-    payload: {},
+    destination: "Log",
+    payload: null
 });
 
-const node = new Node(messages);
-const store = new Store(messages)
+// Logger will be OK
+messages.bind((message) => {
+    console.log(message)
+})
 
-setTimeout(() => {
-    node.ping()
-}, 1000)
+// Pass messages to components
+const node = new Node(messages);
+const net = new Net(messages)
+
+// Enjoy...
+net.ping();
 
