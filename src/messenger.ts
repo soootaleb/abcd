f8053d44efe7a678f4ed0f5e23b8fa4494bbfe22
soootaleb <talebs@icloud.com>
2021-02-04 18:58:04 +0100

    messengers can set source (for worker related things)

--- src/messenger.ts
+++ src/messenger.ts
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file no-explicit-any
 import Observe from "https://deno.land/x/Observe/Observe.ts";
 import { IMessage } from "./interfaces/interface.ts";
 import { EMType } from "./enumeration.ts";
@@ -7,7 +6,7 @@ import { IMPayload } from "./interfaces/mpayload.ts";
 
 export default class Messenger extends Object {
 
-    private messages: Observe<IMessage<EMType>>;
+    protected messages: Observe<IMessage<EMType>>;
 
     constructor(messages: Observe<IMessage<EMType>>) {
         super();
@@ -18,8 +17,9 @@ export default class Messenger extends Object {
             if(message.destination === this.constructor.name) {
                 // Yes, a bit ugly but...
                 // Added deno lint ignore...
+                // deno-lint-ignore no-explicit-any
                 const self: any = this;
-                if (self.hasOwnProperty(message.type)) {
+                if (this.hasOwnProperty(message.type)) {
                     self[message.type](message);
                 } else {
                     this.send(EMType.LogMessage, { message: "Missing handler for " + message.type }, "Log")
@@ -28,65 +28,12 @@ export default class Messenger extends Object {
         })
     }
 
-    protected send<T extends EMType>(type: T, payload: IMPayload[T], destination: string) {
+    protected send<T extends EMType>(type: T, payload: IMPayload[T], destination: string, source?: string) {
         this.messages.setValue({
             type: type,
-            source: this.constructor.name,
+            source: source ? source.toUpperCase().substring(0, 1) + source.substring(1): this.constructor.name,
             destination: destination.toUpperCase().substring(0, 1) + destination.substring(1),
             payload: payload
         })
     }
 }
-
-/**
- * A components extends IO
- * - uses this.send() to send typed messages
- * - declares [EMType.MType] to handle typed messages
- */
-class Node extends Messenger {
-
-    constructor(messages: Observe<IMessage<EMType>>) {
-        super(messages);
-    }
-
-    protected [EMType.InitialMessage]: H<EMType.InitialMessage> = (message) => {
-        this.send(EMType.InitialMessage, null, message.source);
-    }
-}
-
-class Net extends Messenger {
-    constructor(messages: Observe<IMessage<EMType>>) {
-        super(messages);
-    }
-
-    [EMType.InitialMessage]: H<EMType.InitialMessage> = (message) => {
-        this.send(EMType.LogMessage, {
-            message: "Received pong from " + message.source
-        }, "Log")
-    }
-
-    public ping() {
-        this.send(EMType.InitialMessage, null, "Node")
-    }
-}
-
-// // Init messages
-// const messages = new Observe({
-//     type: EMType.InitialMessage,
-//     source: "Root",
-//     destination: "Log",
-//     payload: null
-// });
-
-// // Logger will be OK
-// messages.bind((message) => {
-//     console.log(message)
-// })
-
-// // Pass messages to components
-// const node = new Node(messages);
-// const net = new Net(messages)
-
-// // Enjoy...
-// net.ping();
-
