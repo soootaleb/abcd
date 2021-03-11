7a29224e0d7010831aa93462dcef4428279be73b
soootaleb <sofiane.taleb@sokube.ch>
2021-03-11 11:19:59 +0100

    handle kill signal

--- main.ts
+++ main.ts
@@ -4,3 +4,7 @@ import Node from "./src/node.ts";
 // Register Logger first so eventListener will print message first
 new Logger();
 new Node();
+
+for await (const _ of Deno.signal(Deno.Signal.SIGINT)) {
+  Deno.exit();
+}
