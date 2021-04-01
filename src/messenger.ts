78eaf6e0101aa4582102f4fab36a1000634e762b
soootaleb <talebs@icloud.com>
2021-04-01 20:40:52 +0200

    _doc

--- src/messenger.ts
+++ src/messenger.ts
@@ -3,6 +3,12 @@ import { Args, parse } from "https://deno.land/std/flags/mod.ts";
 import { EComponent, EMType } from "./enumeration.ts";
 import { IMPayload } from "./interfaces/mpayload.ts";
 
+/**
+ * Messenger components can simply implement methods of [EMType.Message] to leverage messages' typing
+ * Message handlers are functions type with H (from types.ts)
+ * 
+ * Messenger components can send messages with the .send() method to get payload typing based on the EMType.Message
+ */
 export default class Messenger extends Object {
   protected args: Args = parse(Deno.args);
   protected worker: Worker | undefined;
@@ -10,6 +16,8 @@ export default class Messenger extends Object {
   constructor(protected state: IState) {
     super();
 
+    // Messenger components subscribe to events of their class name
+    // That's why EComponent must have the same names as the messengers
     addEventListener(this.constructor.name, (ev: Event) => {
       const event: CustomEvent = ev as CustomEvent;
       const message: IMessage<EMType> = event.detail;
@@ -31,6 +39,7 @@ export default class Messenger extends Object {
     const workerfile = new URL(".", import.meta.url).href +
     `workers/${this.constructor.name.toLowerCase()}.worker.ts`;
 
+    // If we detect a worker file, start & register it
     if (Object.keys(EComponent).includes(this.constructor.name + "Worker")) {
       // START THE WORKER
       this.worker = new Worker(workerfile, { type: "module", deno: true });
@@ -60,17 +69,24 @@ export default class Messenger extends Object {
     }
   }
 
+  /**
+   * Allows to send messages with typed payloads
+   * @param type EMType
+   * @param payload Associated payload
+   * @param destination The component to send the message to
+   * @param source if necessary, define the source to override the sender's class name. At your own risk...
+   */
   protected send<T extends EMType>(
     type: T,
     payload: IMPayload[T],
-    destination: EComponent | string,
-    source?: string,
+    destination: EComponent | string, // string is used for peers & clients (IPs)
+    source?: string, // to forward messages transparently like the API
   ) {
     dispatchEvent(
       new CustomEvent(destination, {
         detail: {
           type: type,
-          source: source
+          source: source // By default the source is the class name
             ? source.toUpperCase().substring(0, 1) + source.substring(1)
             : this.constructor.name,
           destination: destination.toUpperCase().substring(0, 1) +
