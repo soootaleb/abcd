a91bc5306617893d05411fc1836d908396df115c
soootaleb <sofiane.taleb@sokube.ch>
2021-04-28 15:45:06 +0200

    [tests] Node

--- tests/node.test.ts
+++ tests/node.test.ts
@@ -1,6 +1,6 @@
 import { state } from "../src/state.ts";
 import Node from "../src/node.ts";
-import { EComponent, EMType, ENodeState } from "../src/enumeration.ts";
+import { EComponent, EKVOpType, EMType, ENodeState, EOpType } from "../src/enumeration.ts";
 import { assertMessages } from "./helpers.ts";
 import { IMessage, IState } from "../src/interfaces/interface.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
@@ -28,26 +28,18 @@ Deno.test("Node::NewState::Follower", async () => {
     source: "Source",
   };
 
-  await assertMessages([], message);
-
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      assertEquals(typeof s.electionTimeoutId === "number", true);
-      assertEquals(s.role, ENodeState.Follower);
-      resolve(true);
-    }, 10);
-  });
-
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      assertEquals(typeof s.electionTimeoutId === "number", false);
-      assertEquals(s.role, ENodeState.Leader);
-      resolve(true);
-    }, 100);
-  });
-
-  clearInterval(s.heartBeatIntervalId);
-  delete s.heartBeatIntervalId;
+  await assertMessages([
+    {
+      type: EMType.NewState,
+      payload: {
+        from: ENodeState.Follower,
+        to: ENodeState.Candidate,
+        reason: `electionTimeout completed (${s.electionTimeout}ms)`
+      },
+      source: EComponent.Node,
+      destination: EComponent.Node,
+    }
+  ], message);
 
   component.shutdown();
 });
@@ -91,18 +83,10 @@ Deno.test("Node::NewState::Leader", async () => {
     },
   ], message);
 
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      assertEquals(typeof s.heartBeatIntervalId === "number", true);
-      assertEquals(s.term, 2);
-      assertEquals(s.role, ENodeState.Leader);
-      assertEquals(s.leader, EComponent.Node);
-      resolve(true);
-    }, 10);
-  });
-
-  clearInterval(s.heartBeatIntervalId);
-  delete s.heartBeatIntervalId;
+  assertEquals(typeof s.heartBeatIntervalId === "number", true);
+  assertEquals(s.term, 2);
+  assertEquals(s.role, ENodeState.Leader);
+  assertEquals(s.leader, EComponent.Node);
 
   component.shutdown();
 });
@@ -213,7 +197,7 @@ Deno.test("Node::CallForVoteRequest::Granted", async () => {
   component.shutdown();
 });
 
-Deno.test("Node::CallForVoteRequest::NotGranted::AlreadyVoted", async () => {
+Deno.test("Node::CallForVoteRequest::AlreadyVoted", async () => {
   const s: IState = {
     ...state,
     voteGrantedDuringTerm: true,
@@ -246,7 +230,7 @@ Deno.test("Node::CallForVoteRequest::NotGranted::AlreadyVoted", async () => {
   component.shutdown();
 });
 
-Deno.test("Node::CallForVoteRequest::NotGranted::Leader", async () => {
+Deno.test("Node::CallForVoteRequest::Leader", async () => {
   const s: IState = {
     ...state,
     role: ENodeState.Leader
@@ -278,7 +262,7 @@ Deno.test("Node::CallForVoteRequest::NotGranted::Leader", async () => {
   component.shutdown();
 });
 
-Deno.test("Node::CallForVoteRequest::NotGranted::OutdatedTerm", async () => {
+Deno.test("Node::CallForVoteRequest::OutdatedTerm", async () => {
   const s: IState = {
     ...state,
     term: 3,
@@ -306,6 +290,170 @@ Deno.test("Node::CallForVoteRequest::NotGranted::OutdatedTerm", async () => {
       },
       source: EComponent.Node,
       destination: message.source,
+    },
+  ], message);
+
+  component.shutdown();
+});
+
+Deno.test("Node::KVOpRejected::Leader", async () => {
+  const s: IState = {
+    ...state,
+    role: ENodeState.Leader
+  };
+
+  const component = new Node(s);
+
+  const request = {
+    token: "token",
+    type: EOpType.KVOp,
+    timestamp: 12345678,
+    payload: {
+      op: EKVOpType.Get,
+      kv: {
+        key: "key"
+      }
+    }
+  }
+
+  const message: IMessage<EMType.KVOpRejected> = {
+    type: EMType.KVOpRejected,
+    destination: EComponent.Node,
+    payload: {
+      reason: "Just because",
+      request: request
+    },
+    source: "Source",
+  };
+
+  await assertMessages([
+    {
+      type: EMType.ClientResponse,
+      payload: request,
+      source: EComponent.Node,
+      destination: EComponent.Node,
+    },
+  ], message);
+
+  component.shutdown();
+});
+
+Deno.test("Node::KVOpRejected::NotLeader", async () => {
+  const s: IState = {
+    ...state,
+    role: ENodeState.Candidate
+  };
+
+  const component = new Node(s);
+
+  const request = {
+    token: "token",
+    type: EOpType.KVOp,
+    timestamp: 12345678,
+    payload: {
+      op: EKVOpType.Get,
+      kv: {
+        key: "key"
+      }
+    }
+  }
+
+  const message: IMessage<EMType.KVOpRejected> = {
+    type: EMType.KVOpRejected,
+    destination: EComponent.Node,
+    payload: {
+      reason: "Just because",
+      request: request
+    },
+    source: "Source",
+  };
+
+  await assertMessages([
+    {
+      type: EMType.LogMessage,
+      payload: {
+        message: "Unexpected KVOpRejected with role " + s.role
+      },
+      source: EComponent.Node,
+      destination: EComponent.Logger,
+    },
+  ], message);
+
+  component.shutdown();
+});
+
+Deno.test("Node::ClientRequestForward", async () => {
+  const s: IState = {
+    ...state,
+    role: ENodeState.Candidate
+  };
+
+  const component = new Node(s);
+
+  const request = {
+    token: "token",
+    type: EOpType.KVOp,
+    timestamp: 12345678,
+    payload: {
+      op: EKVOpType.Get,
+      kv: {
+        key: "key"
+      }
+    }
+  }
+
+  const message: IMessage<EMType.ClientRequestForward> = {
+    type: EMType.ClientRequestForward,
+    destination: EComponent.Node,
+    payload: request,
+    source: "Source",
+  };
+
+  await assertMessages([
+    {
+      type: EMType.ClientRequest,
+      payload: request,
+      source: message.source,
+      destination: EComponent.Api,
+    },
+  ], message);
+
+  component.shutdown();
+});
+
+Deno.test("Node::ClientResponse", async () => {
+  const s: IState = {
+    ...state,
+    role: ENodeState.Candidate
+  };
+
+  const component = new Node(s);
+
+  const request = {
+    token: "token",
+    type: EOpType.KVOp,
+    timestamp: 12345678,
+    payload: {
+      op: EKVOpType.Get,
+      kv: {
+        key: "key"
+      }
+    }
+  }
+
+  const message: IMessage<EMType.ClientResponse> = {
+    type: EMType.ClientResponse,
+    destination: EComponent.Node,
+    payload: request,
+    source: "Source",
+  };
+
+  await assertMessages([
+    {
+      type: EMType.ClientResponse,
+      payload: request,
+      source: EComponent.Node,
+      destination: EComponent.Api,
     },
   ], message);
 
