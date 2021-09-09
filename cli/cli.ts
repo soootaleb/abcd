00ac8720165b1579b03ceab709f06bbcf75bf506
soootaleb <sofiane.taleb@sokube.ch>
2021-09-10 00:53:54 +0200

    try subcommand

--- cli/cli.ts
+++ cli/cli.ts
@@ -10,8 +10,23 @@ await new Command()
   .option("-a, --address <addr>", "HTTP endpoint", { default: "localhost" })
   .option("-p, --port <port>", "HTTP port", { default: 8080 })
   .action(async ({ address, port }: { address: string; port: number }) => {
-    remote = await new Client(address, port).co;
-    await remote.ping().then(console.dir);
-    Deno.exit(0);
+    remote = await new Client(address, port).co.catch(_ => Deno.exit(1));
+    await remote?.ping().then(console.dir);
   })
+  .command("chain", new Command()
+    .description("Interact with blockchain")
+    .version("0.1.0")
+    .option("-f, --from <from:string>", "Source wallet")
+    .option("-t, --to <to:string>", "Destination wallet")
+    .option("-a, --amount <amount:number>", "Amount")
+    .action(async ({ from, to, amount } : {
+      from: string,
+      to: string,
+      amount: number
+    }) => {
+      const response = await remote.chainadd(from, to, amount);
+      console.dir(response);
+      Deno.exit(0)
+    })
+  )
   .parse(Deno.args);
